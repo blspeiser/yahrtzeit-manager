35d989d4cf8a3efdc3a46a6073d8ee8ca92359c7
minidzaldov <mini6731504@gmail.com>
2024-08-01 18:45:31 +0300

    try change the notification - check it

--- lib/services/calendar_service.dart
+++ lib/services/calendar_service.dart
@@ -1,3 +1,61 @@
+// import 'package:device_calendar/device_calendar.dart';
+// import 'package:flutter/services.dart';
+// import 'package:timezone/timezone.dart' as tz;
+
+// class CalendarService {
+//   final DeviceCalendarPlugin _deviceCalendarPlugin = DeviceCalendarPlugin();
+
+//   Future<bool> requestPermissions() async {
+//     try {
+//       var permissionsGranted = await _deviceCalendarPlugin.hasPermissions();
+//       if (permissionsGranted.isSuccess && !permissionsGranted.data!) {
+//         permissionsGranted = await _deviceCalendarPlugin.requestPermissions();
+//         return permissionsGranted.isSuccess && permissionsGranted.data!;
+//       }
+//       return permissionsGranted.data!;
+//     } on PlatformException catch (e) {
+//       print(e);
+//       return false;
+//     }
+//   }
+
+//   Future<List<Calendar>> retrieveCalendars() async {
+//     try {
+//       final calendarsResult = await _deviceCalendarPlugin.retrieveCalendars();
+//       if (calendarsResult.isSuccess) {
+//         return calendarsResult.data ?? [];
+//       } else {
+//         return [];
+//       }
+//     } on PlatformException catch (e) {
+//       print(e);
+//       return [];
+//     }
+//   }
+//   Future<List<Event>> retrieveEventsForToday(String calendarId) async {
+//     try {
+//       final now = tz.TZDateTime.now(tz.local);
+//       final startDate = tz.TZDateTime(tz.local, now.year, now.month, now.day);
+//       final endDate = startDate.add(Duration(days: 1));
+//       final eventsResult = await _deviceCalendarPlugin.retrieveEvents(
+//         calendarId,
+//         RetrieveEventsParams(startDate: startDate, endDate: endDate),
+//       );
+//       if (eventsResult.isSuccess) {
+//         return eventsResult.data ?? [];
+//       } else {
+//         return [];
+//       }
+//     } on PlatformException catch (e) {
+//       print(e);
+//       return [];
+//     }
+//   }
+
+//   Future<void> addEventToCalendar(String calendarId, Event event) async {
+//     await _deviceCalendarPlugin.createOrUpdateEvent(event);
+//   }
+// }
 import 'package:device_calendar/device_calendar.dart';
 import 'package:flutter/services.dart';
 import 'package:timezone/timezone.dart' as tz;
@@ -32,6 +90,7 @@ class CalendarService {
       return [];
     }
   }
+
   Future<List<Event>> retrieveEventsForToday(String calendarId) async {
     try {
       final now = tz.TZDateTime.now(tz.local);
@@ -53,6 +112,10 @@ class CalendarService {
   }
 
   Future<void> addEventToCalendar(String calendarId, Event event) async {
+    // Adding a reminder 10 minutes before the event
+    event.reminders = [Reminder(minutes: 10)];
     await _deviceCalendarPlugin.createOrUpdateEvent(event);
   }
 }
+
+
--- lib/services/yahrtzeits_manager.dart
+++ lib/services/yahrtzeits_manager.dart
@@ -1,12 +1,262 @@
+// import 'package:device_calendar/device_calendar.dart';
+// import 'package:flutter/services.dart';
+// import 'package:timezone/timezone.dart' as tz;
+// import '../models/yahrtzeit.dart';
+// import '../models/yahrtzeit_date.dart';
+
+// class YahrtzeitsManager {
+//   static final YahrtzeitsManager _instance = YahrtzeitsManager._internal();
+//   final List<Yahrtzeit> _yahrtzeits = []; // In-memory storage
+//   final DeviceCalendarPlugin _deviceCalendarPlugin = DeviceCalendarPlugin();
+
+//   static const platform = MethodChannel('com.yahrtzeits/manager');
+
+//   Future<Map<String, dynamic>> nextYahrtzeit(
+//       Map<String, dynamic> yahrtzeit) async {
+//     final result = await platform.invokeMethod<Map<String, dynamic>>(
+//         'nextYahrtzeit', yahrtzeit);
+//     return result!;
+//   }
+
+//   factory YahrtzeitsManager() {
+//     return _instance;
+//   }
+
+//   YahrtzeitsManager._internal();
+
+//   DeviceCalendarPlugin get deviceCalendarPlugin => _deviceCalendarPlugin;
+
+//   Future<void> syncWithCalendar() async {
+//     try {
+//       var permissionsGranted = await _deviceCalendarPlugin.hasPermissions();
+//       print('Permissions Granted: $permissionsGranted');
+//       if (permissionsGranted?.isSuccess == true &&
+//           permissionsGranted?.data == false) {
+//         permissionsGranted = await _deviceCalendarPlugin.requestPermissions();
+//         print('Requested Permissions: $permissionsGranted');
+//         if (permissionsGranted?.isSuccess == false ||
+//             permissionsGranted?.data == false) {
+//           print('Calendar permissions not granted');
+//           return;
+//         }
+//       }
+
+//       final calendarsResult = await _deviceCalendarPlugin.retrieveCalendars();
+//       print('Calendars Result: $calendarsResult');
+//       if (calendarsResult?.isSuccess == true &&
+//           calendarsResult?.data!.isNotEmpty == true) {
+//         _yahrtzeits.clear();
+//         for (var calendar in calendarsResult!.data!) {
+//           print('Retrieving events from calendar: ${calendar.name}');
+//           final eventsResult = await _deviceCalendarPlugin.retrieveEvents(
+//             calendar.id!,
+//             RetrieveEventsParams(
+//               startDate:
+//                   tz.TZDateTime.now(tz.local).subtract(Duration(days: 365)),
+//               endDate: tz.TZDateTime.now(tz.local).add(Duration(days: 365)),
+//             ),
+//           );
+//           if (eventsResult?.isSuccess == true &&
+//               eventsResult?.data!.isNotEmpty == true) {
+//             for (var event in eventsResult!.data!) {
+//               if (event.description?.startsWith('Yahrtzeit for') == true) {
+//                 final hebrewName = _extractHebrewName(event.description!);
+//                 final yahrtzeit = Yahrtzeit(
+//                   englishName: event.title ?? 'Unknown',
+//                   hebrewName: hebrewName,
+//                   day: event.start!.day,
+//                   month: event.start!.month,
+//                   year: event.start!.year,
+//                   gregorianDate: event.start!,
+//                 );
+//                 if (!_yahrtzeits.any((y) =>
+//                     y.englishName == yahrtzeit.englishName &&
+//                     y.hebrewName == yahrtzeit.hebrewName &&
+//                     y.gregorianDate == yahrtzeit.gregorianDate)) {
+//                   _yahrtzeits.add(yahrtzeit);
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       } else {
+//         print('No calendars available or failed to retrieve calendars');
+//       }
+//     } on PlatformException catch (e) {
+//       print('Error syncing with calendar: $e');
+//     }
+//   }
+
+
+//   Future<void> addYahrtzeit(Yahrtzeit yahrtzeit) async {
+//     if (!_yahrtzeits.any((y) =>
+//         y.englishName == yahrtzeit.englishName &&
+//         y.hebrewName == yahrtzeit.hebrewName &&
+//         y.gregorianDate == yahrtzeit.gregorianDate)) {
+//       _yahrtzeits.add(yahrtzeit);
+//       await _addToCalendar(yahrtzeit);
+//     }
+//   }
+
+//   Future<void> updateYahrtzeit(
+//       Yahrtzeit oldYahrtzeit, Yahrtzeit newYahrtzeit) async {
+//     await deleteYahrtzeit(oldYahrtzeit);
+//     await addYahrtzeit(newYahrtzeit);
+//   }
+
+//   Future<void> deleteYahrtzeit(Yahrtzeit yahrtzeit) async {
+//     _yahrtzeits.removeWhere((y) =>
+//         y.englishName == yahrtzeit.englishName &&
+//         y.hebrewName == yahrtzeit.hebrewName &&
+//         y.gregorianDate == yahrtzeit.gregorianDate);
+//     await _deleteFromCalendar(yahrtzeit);
+//   }
+
+//   Future<List<Yahrtzeit>> getAllYahrtzeits() async {
+//     await syncWithCalendar();
+//     return _yahrtzeits;
+//   }
+
+//   Future<List<Yahrtzeit>> getUpcomingYahrtzeits({int days = 10}) async {
+//     final allYahrtzeits = await getAllYahrtzeits();
+//     final now = tz.TZDateTime.now(tz.local);
+//     final upcomingYahrtzeits = allYahrtzeits.where((yahrtzeit) {
+//       final yahrtzeitDate =
+//           tz.TZDateTime.from(yahrtzeit.getGregorianDate(), tz.local);
+//       return yahrtzeitDate.isAfter(now) &&
+//           yahrtzeitDate.isBefore(now.add(Duration(days: days)));
+//     }).toList();
+//     return upcomingYahrtzeits;
+//   }
+
+//   Future<void> _addToCalendar(Yahrtzeit yahrtzeit) async {
+//     try {
+//       var permissionsGranted = await _deviceCalendarPlugin.hasPermissions();
+//       print('Permissions Granted: $permissionsGranted');
+//       if (permissionsGranted?.isSuccess == true &&
+//           permissionsGranted?.data == false) {
+//         permissionsGranted = await _deviceCalendarPlugin.requestPermissions();
+//         print('Requested Permissions: $permissionsGranted');
+//         if (permissionsGranted?.isSuccess == false ||
+//             permissionsGranted?.data == false) {
+//           print('Calendar permissions not granted');
+//           return;
+//         }
+//       }
+
+//       final calendarsResult = await _deviceCalendarPlugin.retrieveCalendars();
+//       print('Calendars Result: $calendarsResult');
+//       if (calendarsResult?.isSuccess == true &&
+//           calendarsResult?.data!.isNotEmpty == true) {
+//         for (var calendar in calendarsResult!.data!) {
+//           print('Adding event to calendar: ${calendar.name}');
+//           final event = Event(
+//             calendar.id!,
+//             title: yahrtzeit.englishName,
+//             description:
+//                 'Yahrtzeit for ${yahrtzeit.englishName} (${yahrtzeit.hebrewName})',
+//             start: tz.TZDateTime.from(yahrtzeit.getGregorianDate(), tz.local),
+//             end: tz.TZDateTime.from(yahrtzeit.getGregorianDate(), tz.local)
+//                 .add(Duration(hours: 1)),
+//           );
+//           print(
+//               'Event Details: ${event.title}, ${event.description}, ${event.start}, ${event.end}');
+//           final result = await _deviceCalendarPlugin.createOrUpdateEvent(event);
+//           print(
+//               'Create or Update Event Result for ${calendar.name}: ${result?.data}');
+//           if (result?.isSuccess == false) {
+//             print(
+//                 'Error creating or updating event for ${calendar.name}: ${result?.data}');
+//           }
+//         }
+//       } else {
+//         print('No calendars available or failed to retrieve calendars');
+//       }
+//     } on PlatformException catch (e) {
+//       print('Error adding event to calendar: $e');
+//     }
+//   }
+
+//   Future<void> _deleteFromCalendar(Yahrtzeit yahrtzeit) async {
+//     try {
+//       final calendarsResult = await _deviceCalendarPlugin.retrieveCalendars();
+//       if (calendarsResult?.isSuccess == true &&
+//           calendarsResult?.data!.isNotEmpty == true) {
+//         for (var calendar in calendarsResult!.data!) {
+//           final eventsResult = await _deviceCalendarPlugin.retrieveEvents(
+//             calendar.id!,
+//             RetrieveEventsParams(
+//               startDate:
+//                   tz.TZDateTime.now(tz.local).subtract(Duration(days: 365)),
+//               endDate: tz.TZDateTime.now(tz.local).add(Duration(days: 365)),
+//             ),
+//           );
+//           if (eventsResult?.isSuccess == true &&
+//               eventsResult?.data!.isNotEmpty == true) {
+//             for (var event in eventsResult!.data!) {
+//               if (event.description?.startsWith(
+//                       'Yahrtzeit for ${yahrtzeit.englishName} (${yahrtzeit.hebrewName})') ==
+//                   true) {
+//                 await _deviceCalendarPlugin.deleteEvent(
+//                     calendar.id!, event.eventId!);
+//               }
+//             }
+//           }
+//         }
+//       }
+//     } on PlatformException catch (e) {
+//       print('Error deleting event from calendar: $e');
+//     }
+//   }
+
+//   List<YahrtzeitDate> nextMultiple(List<Yahrtzeit> yahrtzeits) {
+//     final dates = yahrtzeits
+//         .map((yahrtzeit) => YahrtzeitDate.fromYahrtzeit(yahrtzeit))
+//         .toList();
+//     dates.sort((a, b) => a.gregorianDate.compareTo(b.gregorianDate));
+//     return dates;
+//   }
+
+//   String _extractHebrewName(String description) {
+//     final regex = RegExp(r'\((.*?)\)');
+//     final match = regex.firstMatch(description);
+//     return match != null ? match.group(1)! : 'Unknown';
+//   }
+//     String _extractEnglishName(String description) {
+//     final regex = RegExp(r'Group Yahrtzeit for (.*?) \(');
+//     final match = regex.firstMatch(description);
+//     return match != null ? match.group(1)! : 'Unknown';
+//   }
+
+
+//    bool _compareYahrtzeitLists(List<Yahrtzeit> list1, List<Yahrtzeit> list2) {
+//     if (list1.length != list2.length) {
+//       return false;
+//     }
+//     for (var yahrtzeit in list1) {
+//       if (!list2.any((element) =>
+//           element.englishName == yahrtzeit.englishName &&
+//           element.hebrewName == yahrtzeit.hebrewName &&
+//           element.gregorianDate == yahrtzeit.gregorianDate)) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+
+// }
+
 import 'package:device_calendar/device_calendar.dart';
 import 'package:flutter/services.dart';
 import 'package:timezone/timezone.dart' as tz;
 import '../models/yahrtzeit.dart';
 import '../models/yahrtzeit_date.dart';
+import '../models/group.dart';
 
 class YahrtzeitsManager {
   static final YahrtzeitsManager _instance = YahrtzeitsManager._internal();
   final List<Yahrtzeit> _yahrtzeits = []; // In-memory storage
+  final List<Group> _groups = []; // In-memory storage for groups
   final DeviceCalendarPlugin _deviceCalendarPlugin = DeviceCalendarPlugin();
 
   static const platform = MethodChannel('com.yahrtzeits/manager');
@@ -87,7 +337,6 @@ class YahrtzeitsManager {
     }
   }
 
-
   Future<void> addYahrtzeit(Yahrtzeit yahrtzeit) async {
     if (!_yahrtzeits.any((y) =>
         y.englishName == yahrtzeit.englishName &&
@@ -158,6 +407,7 @@ class YahrtzeitsManager {
             start: tz.TZDateTime.from(yahrtzeit.getGregorianDate(), tz.local),
             end: tz.TZDateTime.from(yahrtzeit.getGregorianDate(), tz.local)
                 .add(Duration(hours: 1)),
+            reminders: [Reminder(minutes: 1440)], // Reminder 1 day before
           );
           print(
               'Event Details: ${event.title}, ${event.description}, ${event.start}, ${event.end}');
@@ -226,22 +476,6 @@ class YahrtzeitsManager {
     final regex = RegExp(r'Group Yahrtzeit for (.*?) \(');
     final match = regex.firstMatch(description);
     return match != null ? match.group(1)! : 'Unknown';
-  }
-
-
-   bool _compareYahrtzeitLists(List<Yahrtzeit> list1, List<Yahrtzeit> list2) {
-    if (list1.length != list2.length) {
-      return false;
     }
-    for (var yahrtzeit in list1) {
-      if (!list2.any((element) =>
-          element.englishName == yahrtzeit.englishName &&
-          element.hebrewName == yahrtzeit.hebrewName &&
-          element.gregorianDate == yahrtzeit.gregorianDate)) {
-        return false;
-      }
-    }
-    return true;
-  }
-
+  
 }
