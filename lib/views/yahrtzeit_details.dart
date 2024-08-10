b96535e41b6779038be160f3ff3168b4baaa45d4
RachelWiner <r.96769@gmail.com>
2024-08-11 02:10:29 +0300

    Share from upcoming complete

--- lib/views/yahrtzeit_details.dart
+++ lib/views/yahrtzeit_details.dart
@@ -1,99 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:kosher_dart/kosher_dart.dart';
-// import 'package:share/share.dart';
-// import '../localizations/app_localizations.dart';
-// import '../models/yahrtzeit_date.dart';
-// import 'package:intl/intl.dart';
-
-// class YahrtzeitDetailsPage extends StatelessWidget {
-//   final YahrtzeitDate yahrtzeitDate;
-
-//   YahrtzeitDetailsPage({required this.yahrtzeitDate});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final gregorianFormatter = DateFormat('MMMM d, yyyy');
-//     final hebrewFormatter = HebrewDateFormatter()
-//       ..hebrewFormat = true
-//       ..useGershGershayim = true;
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(
-//             '${yahrtzeitDate.yahrtzeit.englishName} ${AppLocalizations.of(context)!.translate('Details')}',
-//             style: TextStyle(color: Colors.white)),
-//         centerTitle: true,
-//         backgroundColor: Color.fromARGB(255, 50, 4, 129),
-//         elevation: 0,
-//         actionsIconTheme: IconThemeData(color: Colors.white),
-//         actions: [
-//           IconButton(
-//             icon: Icon(Icons.share),
-//             onPressed: () {
-//               Share.share(
-//                 'Yahrtzeit Details:\n\n'
-//                 'English Name: ${yahrtzeitDate.yahrtzeit.englishName}\n'
-//                 'Hebrew Name: ${yahrtzeitDate.yahrtzeit.hebrewName}\n'
-//                 'Gregorian Date: ${gregorianFormatter.format(yahrtzeitDate.gregorianDate)}\n'
-//                 'Hebrew Date: ${hebrewFormatter.format(yahrtzeitDate.hebrewDate)}',
-//               );
-//             },
-//           ),
-//         ],
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Card(
-//           elevation: 5,
-//           shape:
-//               RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-//           child: Padding(
-//             padding: const EdgeInsets.all(16.0),
-//             child: Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 _buildDetailRow(
-//                     AppLocalizations.of(context)!.translate('English Name'),
-//                     yahrtzeitDate.yahrtzeit.englishName!),
-//                 _buildDetailRow(
-//                     AppLocalizations.of(context)!.translate('Hebrew Name'),
-//                     yahrtzeitDate.yahrtzeit.hebrewName),
-//                 _buildDetailRow(
-//                     AppLocalizations.of(context)!.translate('gregorian_date'),
-//                     gregorianFormatter.format(yahrtzeitDate.gregorianDate)),
-//                 _buildDetailRow(
-//                     AppLocalizations.of(context)!.translate('hebrew_gate'),
-//                     hebrewFormatter.format(yahrtzeitDate.hebrewDate)),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-
-//   Widget _buildDetailRow(String label, String value) {
-//     return Padding(
-//       padding: const EdgeInsets.symmetric(vertical: 8.0),
-//       child: Row(
-//         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//         children: [
-//           Text(
-//             '$label:',
-//             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-//           ),
-//           Flexible(
-//             child: Text(
-//               value,
-//               style: TextStyle(fontSize: 18, color: Colors.grey[600]),
-//               textAlign: TextAlign.right,
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:kosher_dart/kosher_dart.dart';
 import 'package:share/share.dart';
@@ -174,42 +78,34 @@ class YahrtzeitDetailsPage extends StatelessWidget {
     );
   }
 
-  String _createICSContent(YahrtzeitDate yahrtzeitDate) {
-    final buffer = StringBuffer();
 
-    buffer.writeln("BEGIN:VCALENDAR");
-    buffer.writeln("VERSION:2.0");
-    buffer.writeln("PRODID:-//YourApp//Yahrtzeit Manager//EN");
+  String _createICSContent(YahrtzeitDate yahrtzeitDate) {
+  final buffer = StringBuffer();
+
+  buffer.writeln("BEGIN:VCALENDAR");
+  buffer.writeln("VERSION:2.0");
+  buffer.writeln("PRODID:-//YourApp//Yahrtzeit Manager//EN");
 
     try {
-      // Convert Jewish date to Gregorian
-      final jewishDate = JewishDate.initDate(
-        jewishYear: yahrtzeitDate.yahrtzeit.day!,
-        jewishMonth: yahrtzeitDate.yahrtzeit.month!,
-        jewishDayOfMonth: yahrtzeitDate.yahrtzeit.day!,
-      );
-      final gregorianDate = DateTime(
-        jewishDate.getGregorianYear(),
-        jewishDate.getGregorianMonth(),
-        jewishDate.getGregorianDayOfMonth(),
-      );
 
-      final start = DateFormat("yyyyMMdd").format(gregorianDate);
+      final start = DateFormat("yyyyMMdd'T'HHmmss'Z'").format(yahrtzeitDate.gregorianDate);
+      final end = DateFormat("yyyyMMdd'T'HHmmss'Z'")
+          .format(yahrtzeitDate.gregorianDate.add(Duration(hours: 2)));
+
       buffer.writeln("BEGIN:VEVENT");
-      buffer.writeln("UID:${yahrtzeitDate.yahrtzeit.id}");
       buffer.writeln("SUMMARY:${yahrtzeitDate.yahrtzeit.englishName ?? yahrtzeitDate.yahrtzeit.hebrewName}");
       buffer.writeln("DTSTART:$start");
-      buffer.writeln("DURATION:PT24H"); // Event duration 24 hours
+      buffer.writeln("DTEND:$end");
+      buffer.writeln("DESCRIPTION:Yahrtzeit for ${yahrtzeitDate.yahrtzeit.englishName ?? yahrtzeitDate.yahrtzeit.hebrewName}");
       buffer.writeln("END:VEVENT");
     } catch (e) {
       print("Error with Yahrtzeit: ${yahrtzeitDate.yahrtzeit.englishName ?? yahrtzeitDate.yahrtzeit.hebrewName}, Error: $e");
-      // Optionally handle the error here
     }
 
-    buffer.writeln("END:VCALENDAR");
+  buffer.writeln("END:VCALENDAR");
 
-    return buffer.toString();
-  }
+  return buffer.toString();
+}
 
   Widget _buildDetailRow(String label, String value) {
     return Padding(
